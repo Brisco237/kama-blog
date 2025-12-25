7aa3feb741cf1b7f22f82d3f7ceceef4d23764be
kamdem Brice <kamdembrice770@gmail.com>
2025-12-25 20:22:12 +0100

    First migrations

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+AUTH_USER_MODEL = "authapp.User"
+#LOGIN_URL = '/authapp/login_user/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
