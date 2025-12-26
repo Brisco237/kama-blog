f1d32705ac06e653e7f73ae34dd2de2099d4b4fd
kamdem Brice <kamdembrice770@gmail.com>
2025-12-26 06:08:00 +0100

    Navbar terminé

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articles.urls')),
+    path('', home, name='home'),
+    path('articles/', include('articles.urls')),
     path('auth/', include('authapp.urls')),
 ]
