7fd17108578b1e347708e0eaa8164ed4b9fd3906
kamdem Brice <kamdembrice770@gmail.com>
2025-12-26 12:15:23 +0100

    dernier commit avant de débuter avec le footer

--- blog/authapp/views.py
+++ blog/authapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from articles.models import Article
+from articles.models import Article, Category
 
 # Create your views here.
 def home(request):
     articles = Article.objects.filter(status='published').order_by('published_at')
-    article3 = Article.objects.all().first()
-    article2 = articles[1] 
+    categories = Category.objects.all()
     article1 = articles[0]
+    article2 = articles[1] 
+    article3 = articles[2]
     article4 = articles[3]
     article5 = articles[4]
     return render(request, 'authapp/home.html',
-    {'article1': article1, 'article2': article2, 'article3': article3, 'article4': article4, 'article5': article5}
+    {'article1': article1, 'article2': article2, 'article3': article3, 
+    'article4': article4, 'article5': article5, 'categories': categories
+    }
     )
