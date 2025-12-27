89504b831d0292efe20b1cb68adc802d6486dbff
kamdem Brice <kamdembrice770@gmail.com>
2025-12-27 18:23:41 +0100

    creation de article_list et article_detail

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import archives
+from .views import archives, ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('archives/', archives, name='archives'),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from .models import Category, Article
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def archives(request):
     articles = Article.objects.all()
     nb_article = articles.count()
     categories = Category.objects.all()
-    return render(request, 'articles/archives.html', {'categories': categories, 'nb_article' : nb_article, 'articles' : articles})
+    return render(request, 'articles/archives.html', 
+    {'categories': categories, 'nb_article' : nb_article, 
+    'articles' : articles
+    }
+    )
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/article_list.html'
+    context_object_name = 'articles'
+    paginate_by = 6
+
+    #def get_queryset(self):
+     #   return Article.objects.filter(published_at=True)
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/article_detail.html'
+    context_object_name = 'article'
