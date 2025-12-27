f4f5cf93c45a0124614a3979247c3835a6faf203
kamdem Brice <kamdembrice770@gmail.com>
2025-12-27 14:28:46 +0100

    archives pages progression

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def archives(request):
-    return render(request, 'articles/archives.html')
+    categories = Category.objects.all()
+    return render(request, 'articles/archives.html', {'categories': categories})
