2062bcd61f2e43416a987e8f97ad6238e448a9d7
kamdem Brice <kamdembrice770@gmail.com>
2025-12-27 16:09:40 +0100

    page archives terminé

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Article
 
 # Create your views here.
 def archives(request):
+    articles = Article.objects.all()
+    nb_article = articles.count()
     categories = Category.objects.all()
-    return render(request, 'articles/archives.html', {'categories': categories})
+    return render(request, 'articles/archives.html', {'categories': categories, 'nb_article' : nb_article, 'articles' : articles})
