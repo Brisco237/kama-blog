2f04cd93c299ba2410c4682bd4f925eb58256c3b
kamdem Brice <kamdembrice770@gmail.com>
2026-01-09 03:57:31 +0100

    Le gros du boulot a déja été fait

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Category, Article
+from django.shortcuts import render, redirect
+from .models import Category, Article, Comment
 from django.views.generic import ListView, DetailView
+from django.contrib import messages
 
 # Create your views here.
 def archives(request):
@@ -27,9 +28,24 @@ class ArticleDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         article = self.object
-
         context['related_articles'] = (Article.objects.filter(category=article.category).exclude(id=article.id))
-
         return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        
+        if not request.user.is_authenticated:
+            messages.error(request, 'Vous devez être connecté pour ajouter un commentaire.')
+            return redirect('article_detail', slug=self.object.slug)
+    
+        commentaire = request.POST.get('commentaire', '').strip()
+        
+        if not commentaire:
+            messages.error(request, 'Le commentaire ne peut pas être vide.')
+            return redirect('article_detail', slug=self.object.slug)
+    
+        Comment.objects.create(article=self.object,user=request.user,content=commentaire)
+        messages.success(request, 'Votre commentaire a été ajouté avec succès!')
+        return redirect('article_detail', slug=self.object.slug)
+
